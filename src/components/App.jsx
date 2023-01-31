5d493626bf3273b2a0a5da8a27984732a0e1a64f
OleksiiChernii <oleksii.chernii@gmail.com>
2023-01-31 21:14:43 +0200

    added func...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { ContactsList } from './ContactsList';
+import { CreateContact } from './CreateContact';
+import { FilterContacts } from './FilterContacts';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <CreateContact />
+      <ContactsList />
+      <FilterContacts />
+    </>
   );
 };
---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contactsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
+// export const store = configureStore({ reducer: contactsReducer });
